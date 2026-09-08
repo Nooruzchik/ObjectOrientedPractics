c05097f305660d7c99fd9859a44f61f3f5c9b92a
Nooruz Suleimanov <suleimanovnooruz@gmail.com>
2026-09-08 20:06:26 +0700

    Add project files.

---
+++ ObjectOrientedPractics/Model/Customer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    internal class Customer
+    {
+        private readonly int _id;
+        private string _fullname;
+        private string _address;
+
+
+        public string FullName
+        {
+            get {  return _fullname; }
+            set
+            {
+                if (_fullname.Length > 200)
+                    throw new ArgumentException("ФИО не должно превыышать 200 сим");
+                _fullname = value;
+            }
+        }
+
+        public string Address
+        {
+            get { return _address; }
+            set
+            {
+                if (_address.Length > 500)
+                    throw new ArgumentException("Адреес не должен превышать 500 сим");
+                _address = value;
+            }
+        }
+
+        public Customer(string _fullname, string _address)
+        {
+            FullName = _fullname;
+            Address = _address;
+        }
+    }
+}
---
+++ ObjectOrientedPractics/Model/Item.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ObjectOrientedPractics.Model
+{
+    internal class Item
+    {
+
+        private readonly int _id;
+        private string _name; 
+        private string _info;
+        private double _cost;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name.Length < 1 || _name.Length > 200)
+                {
+                    throw new ArgumentException("Длина имени должна быть от 1 до 200 символов");
+                }
+                _name = value;
+            }
+        }
+
+        public string Info
+        {
+            get { return _info; }
+            set
+            {
+                if (_name.Length > 1000)
+                {
+                    throw new ArgumentException("Длинна описание товара может быть не больше 1000 символов"); 
+                }
+                _info = value;
+            }
+        }
+
+        public double Cost
+        {
+            get { return _cost; }
+            set
+            {
+                if (_cost < 0 || _cost > 100000)
+                {
+                    throw new ArgumentException("Цена товара может быть от 0 до 100000");
+                }
+                _cost = value;
+            }
+        }
+
+        public Item(string _name,  string _info, double _cost)
+        {
+            Name = _name;
+            Info = _info;
+            Cost = _cost;
+        }
+    }
+}
